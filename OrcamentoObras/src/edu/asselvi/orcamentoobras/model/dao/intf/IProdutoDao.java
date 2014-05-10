0f32880c6f708dbc7b46feaacc050897e73149cc
LeandroRebelo <leandroblu9@gmail.com>
2014-05-10 15:00:30 -0300

    Criação da IProdutoDao e implementação padrão

---
+++ OrcamentoObras/src/edu/asselvi/orcamentoobras/model/dao/intf/IProdutoDao.java
@@ -0,0 +1,25 @@
+package edu.asselvi.orcamentoobras.model.dao.intf;
+
+import java.sql.SQLException;
+
+import edu.asselvi.orcamentoobras.model.Produto;
+
+/**
+ * Comportamento padrão para toda DAO de {@link Produto}
+ * 
+ * @author Leandro Rebelo
+ * 
+ */
+
+public interface IProdutoDao extends IDao<Produto> {
+
+	/**
+	 * Retorna {@link Produto} considerando o identificador do mesmo
+	 * 
+	 * @param id
+	 * @return {@link Produto}
+	 * @throws SQLExpection
+	 */
+	public Produto getPeloCodigo(Integer codigo) throws SQLException;
+
+}
