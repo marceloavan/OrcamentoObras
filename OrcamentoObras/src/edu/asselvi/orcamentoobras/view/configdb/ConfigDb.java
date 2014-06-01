175a4c9c2aa8cf984199f6f39df1f95ec213b54f
LeandroRebelo <leandroblu9@gmail.com>
2014-06-01 17:57:24 -0300

    Criação da tela de Configuração do Banco de Dados

---
+++ OrcamentoObras/src/edu/asselvi/orcamentoobras/view/configdb/ConfigDb.java
@@ -0,0 +1,22 @@
+package edu.asselvi.orcamentoobras.view.configdb;
+
+import edu.asselvi.orcamentoobras.view.templates.GeneralTemplate;
+
+public class ConfigDb extends GeneralTemplate {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ConfigDb() {
+		super(800, 800);
+	}
+
+	@Override
+	protected void addActions() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
