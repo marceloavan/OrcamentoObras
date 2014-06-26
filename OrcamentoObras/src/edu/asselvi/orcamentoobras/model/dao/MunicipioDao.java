76e35b1441c42b85964db3e905dbd2d986b00256
LeandroRebelo <leandroblu9@gmail.com>
2014-06-25 23:29:02 -0300

    Implementação do método createTable

--- OrcamentoObras/src/edu/asselvi/orcamentoobras/model/dao/MunicipioDao.java
+++ OrcamentoObras/src/edu/asselvi/orcamentoobras/model/dao/MunicipioDao.java
@@ -11,16 +11,16 @@ import edu.asselvi.orcamentoobras.model.beans.UnidadeFederativa;
 import edu.asselvi.orcamentoobras.model.dao.intf.IMunicipioDao;
 
 public class MunicipioDao extends AbstractDao implements IMunicipioDao {
-	
+
 	@Deprecated
 	@Override
 	public void inserir(Municipio param) throws SQLException {
-		
+
 		StringBuilder sb = new StringBuilder();
 		sb.append("INSERT INTO MUNICIPIO");
 		sb.append(" (COD_MUNICIPIO, DESCRICAO, COD_UF)");
 		sb.append(" VALUES (?, ?, ?)");
-		
+
 		String sql = sb.toString();
 		PreparedStatement stmt = null;
 		try {
@@ -28,16 +28,17 @@ public class MunicipioDao extends AbstractDao implements IMunicipioDao {
 			stmt.setLong(1, param.getCodigo());
 			stmt.setString(2, param.getDescricao());
 			stmt.setInt(3, param.getUf().getCodigo());
-			
+
 			stmt.execute();
-			
+
 			int linhasAfetadas = stmt.executeUpdate();
-	        if (linhasAfetadas == 0) {
-	            throw new SQLException("Falha ao criar registro");
-	        }
-	        
+			if (linhasAfetadas == 0) {
+				throw new SQLException("Falha ao criar registro");
+			}
+
 		} finally {
-			if (stmt != null) stmt.close();
+			if (stmt != null)
+				stmt.close();
 			getConexao().close();
 		}
 	}
@@ -45,39 +46,43 @@ public class MunicipioDao extends AbstractDao implements IMunicipioDao {
 	@Deprecated
 	@Override
 	public void atualizar(Municipio param) throws SQLException {
-		throw new SQLException("Não será necessário atualizar Municipio, todos estão mapeadas");
+		throw new SQLException(
+				"Nï¿½o serï¿½ necessï¿½rio atualizar Municipio, todos estï¿½o mapeadas");
 	}
 
 	@Deprecated
 	@Override
 	public void remover(Municipio param) throws SQLException {
-		throw new SQLException("Não será necessário remover Municipio, todos estão mapeadas");
+		throw new SQLException(
+				"Nï¿½o serï¿½ necessï¿½rio remover Municipio, todos estï¿½o mapeadas");
 	}
 
 	@Deprecated
 	@Override
 	public List<Municipio> getTodos() throws SQLException {
-		
+
 		List<Municipio> municipioLista = new ArrayList<Municipio>();
 
 		StringBuilder sb = new StringBuilder();
 		sb.append("SELECT * FROM MUNICIPIO");
 		String sql = sb.toString();
-		
+
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		try {
 			stmt = getConexao().prepareStatement(sql);
 			rs = stmt.executeQuery();
-			
+
 			while (rs.next()) {
 				Integer codigoMunicipio = rs.getInt("COD_MUNICIPIO");
 				String descricao = rs.getString("DESCRICAO");
 				Integer codigoUf = rs.getInt("COD_UF");
-				UnidadeFederativa uf = getDaoFactory().getUnidadeFederativaDao().getPeloCodigo(codigoUf);
-				municipioLista.add(new Municipio(codigoMunicipio, descricao, uf));
+				UnidadeFederativa uf = getDaoFactory()
+						.getUnidadeFederativaDao().getPeloCodigo(codigoUf);
+				municipioLista
+						.add(new Municipio(codigoMunicipio, descricao, uf));
 			}
-			
+
 		} finally {
 			finalizarConexoes(stmt, rs);
 		}
@@ -85,24 +90,26 @@ public class MunicipioDao extends AbstractDao implements IMunicipioDao {
 	}
 
 	@Override
-	public List<Municipio> getTodosDaUf(UnidadeFederativa uf) throws SQLException {
-		
+	public List<Municipio> getTodosDaUf(UnidadeFederativa uf)
+			throws SQLException {
+
 		List<Municipio> municipioLista = new ArrayList<Municipio>();
 
 		String sql = "SELECT * FROM MUNICIPIO WHERE COD_UF = ?";
-		
+
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		try {
 			stmt = getConexao().prepareStatement(sql);
 			rs = stmt.executeQuery();
-			
+
 			while (rs.next()) {
 				Integer codigoMunicipio = rs.getInt("COD_MUNICIPIO");
 				String descricao = rs.getString("DESCRICAO");
-				municipioLista.add(new Municipio(codigoMunicipio, descricao, uf));
+				municipioLista
+						.add(new Municipio(codigoMunicipio, descricao, uf));
 			}
-			
+
 		} finally {
 			finalizarConexoes(stmt, rs);
 		}
@@ -116,23 +123,45 @@ public class MunicipioDao extends AbstractDao implements IMunicipioDao {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		try {
-			
+
 			stmt = getConexao().prepareStatement(sql);
 			stmt.setInt(1, codigoMunicipio);
 			rs = stmt.executeQuery();
-			
+
 			Municipio municipio = null;
 			if (rs.next()) {
 				Integer codigo = codigoMunicipio;
 				String descricao = rs.getString("DESCRICAO");
 				Integer codigoUf = rs.getInt("COD_UF");
-				UnidadeFederativa uf = getDaoFactory().getUnidadeFederativaDao().getPeloCodigo(codigoUf);
+				UnidadeFederativa uf = getDaoFactory()
+						.getUnidadeFederativaDao().getPeloCodigo(codigoUf);
 				municipio = new Municipio(codigo, descricao, uf);
 			}
 			return municipio;
-			
+
 		} finally {
 			finalizarConexoes(stmt, rs);
 		}
+	}
+
+	@Override
+	public void createTable(Municipio param) throws SQLException {
+		String sql = "CREATE TABLE MUNICIPIO (COD_MUNICIPIO INTEGER NOT NULL, DESCRICAO VARCHAR(100) NOT NULL, "
+				+ "COD_UF INTEGER NOT NULL, CONSTRAINT PK_MUNICIPIO PRIMARY KEY (COD_MUNICIPIO), "
+				+ "CONSTRAINT FK_MUNICIPIO_UNIDADE_FEDERATIVA FOREIGN KEY (COD_UF) REFERENCES UNIDADE_FEDERATIVA(COD_UF)";
+		
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			stmt = getConexao().prepareStatement(sql);
+			rs = stmt.executeQuery();
+			
+		} finally {
+			
+			finalizarConexoes(stmt, rs);
+		}
+
 	}
 }
