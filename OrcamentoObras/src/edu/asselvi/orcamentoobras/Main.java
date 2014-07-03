a3e2e3ffed610d7be01a0548a1111da5b15589d1
Marcelo <marcelo.avan@gmail.com>
2014-07-03 00:36:20 -0300

    Correção do Main para abrir a tela de configuração de banco caso falhar

--- OrcamentoObras/src/edu/asselvi/orcamentoobras/Main.java
+++ OrcamentoObras/src/edu/asselvi/orcamentoobras/Main.java
@@ -1,12 +1,41 @@
 package edu.asselvi.orcamentoobras;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
+import edu.asselvi.orcamentoobras.model.dao.conector.ConectorBancoFactory;
 import edu.asselvi.orcamentoobras.view.login.Login;
-
+import edu.asselvi.orcamentoobras.view.pages.ConfigDbPage;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		boolean failureConnection = false;
+		Connection connection;
+		try {
+			connection = ConectorBancoFactory.getInstance().getConexao();
+			if (connection == null) {
+				failureConnection = true;
+			}
+		} catch (SQLException e) {
+			failureConnection = true;
+		}
+		
+		if (failureConnection) {
+			SwingUtilities.invokeLater(new Runnable() {
+		        public void run() {
+		        	new ConfigDbPage();
+		        	JOptionPane.showMessageDialog(null, "Não possível conectar com a base de dados configurada atualmente.\n"
+		        			+ "Verifique a configuração atual.\n"
+		        			+ "Caso não saiba do que se trata, contacte o suporte tecnico.");
+		        }
+		    });
+			return;
+		}
+		
 		SwingUtilities.invokeLater(new Runnable() {
 	        public void run() {
 	        	new Login();
